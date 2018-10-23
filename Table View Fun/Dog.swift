4b80e78101e509846c9d6bdac36af21ad96de4cc
Gina Sprint <sprint@gonzaga.edu>
2018-10-23 11:49:41 -0700

    Add author credit for icon from flaticon.com

--- Table View Fun/Dog.swift
+++ Table View Fun/Dog.swift
@@ -19,6 +19,10 @@ class Dog {
         self.name = name
         self.breed = breed
         // MARK: LAB #21
+        // Author credit for image
+        /*
+         <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        */
         self.imageName = "dog"
     }
     
